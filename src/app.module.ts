c01f6604d1ff646225305993e7c9d0dbdb3b36a5
Teagan glenn <that@teagantotally.rocks>
2023-11-28 00:33:10 -0700

    Update service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,38 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AppConfig } from "./app.config";
-import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
   imports: [ConfigModule.forFeature(AppConfig)],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+/**
+ * import { Module } from "@nestjs/common";
+import { TypedConfigModule, dotenvLoader, fileLoader } from "nest-typed-config";
+import { AppService } from "./app.service";
+import { RootConfig } from "./config";
+
+@Module({
+  imports: [
+    TypedConfigModule.forRoot({
+      schema: RootConfig,
+      load: [
+        fileLoader({
+          absolutePath: "./config.yaml",
+          ignoreEnvironmentVariableSubstitution: false,
+        }),
+        dotenvLoader({
+          envFilePath: "./.env",
+          expandVariables: true,
+        }),
+      ],
+    }),
+  ],
+  providers: [AppService],
+})
+export class AppModule {}
+
+ */
--- src/app.service.ts
+++ src/app.service.ts
@@ -15,6 +15,13 @@ const sleep = (ms: number) =>
     setTimeout(() => resolve(), ms);
   });
 
+type OfferDetails = {
+  storeName: string;
+  offerAmount: number;
+  offerUnit: string;
+  offerType: string;
+};
+
 @Injectable()
 export class AppService {
   private driver: WebDriver;
@@ -39,32 +46,59 @@ export class AppService {
     await this.enterCredentials();
   }
 
-  async addOffers() {
+  async getOfferDetails(): Promise<OfferDetails> {
+    const storeElement = await selectors.offers.offerName.exec(this.driver);
+    const offerElement = await selectors.offers.offerAmount.exec(this.driver);
+    const storeName = await storeElement.getText();
+    const offer = await offerElement.getText();
+    const offerAmount = Number.parseInt(
+      offer.match(/(?<amount>\d+)/).groups!["amount"]
+    );
+    const offerUnit = offer.match(/(?<unit>[\$%])/).groups!["unit"];
+    const offerType = offer.match(/%? (?<type>[a-zA-Z].+)$/).groups!["type"];
+    return {
+      storeName,
+      offerAmount,
+      offerType,
+      offerUnit,
+    };
+  }
+
+  async addOffers(): Promise<OfferDetails[]> {
+    const newOffers: OfferDetails[] = [];
     let offerButton: WebElement | null;
     while (
       (offerButton = await selectors.offers.addOffer.exec(this.driver)) !== null
     ) {
+      const offerDetails = await this.getOfferDetails();
+      newOffers.push(offerDetails);
       await selectors.offers.backButton.exec(this.driver);
     }
+    return newOffers;
   }
 
-  async processCards() {
+  async processCards(): Promise<Record<string, OfferDetails[]>> {
     console.log("Waiting for card dropdown selector...");
+    const newOffers: Record<string, OfferDetails[]> = {};
     let cardCount = 0;
     let cardIndex = 0;
     do {
       const dropdown = await selectors.account.cardSelect.exec(this.driver);
-      cardCount = await dropdown
-        .findElements(By.css("mds-select-option"))
-        .then((elements) => {
-          return elements.length;
-        });
-      await selectors.account.cardSelectOption
+      const elements = await dropdown.findElements(By.css("mds-select-option"));
+      cardCount = elements.length;
+      const cardText = await selectors.account.cardSelectOption
         .replaceWith(`${cardIndex + 1}`)
-        .exec(this.driver);
-      await this.addOffers();
+        .exec(this.driver, async (element: WebElement) => {
+          const text = await element.getText();
+          await element.click();
+          return text;
+        });
+      const card = cardText.match(/^(?<card>[^,]+)/).groups!["card"];
+      newOffers[card] = await this.addOffers();
       cardIndex++;
     } while (cardIndex < cardCount);
+
+    return newOffers;
   }
 
   async navigateToOffers() {
@@ -82,7 +116,8 @@ export class AppService {
       await this.driver.navigate().to("https://chase.com");
       await this.authenticate();
       await this.navigateToOffers();
-      await this.processCards();
+      const newOffers = await this.processCards();
+      console.dir(newOffers, { depth: 5 });
     } finally {
       await this.driver.close();
     }
